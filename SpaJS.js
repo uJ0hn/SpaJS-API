3e1720e39e9420269c7adcb0a0c9289ddc3f65c1
eomuhzin <pessoalmurillofrazao@gmail.com>
2024-01-23 18:14:36 -0300

    commit

--- SpaJS.js
+++ SpaJS.js
@@ -15,11 +15,16 @@ class SpaJS {
         })
     }
 
+
     get(path, callback, page) {
         this.app.get(path, (req, res) => {
             if (req.query["javascript"] === undefined && req.headers["javascript"] === undefined) {
                 res.sendFile(p.join(__dirname, `public/index.html`));
             } else {
+                /**
+                 * @param {import('express').Request} req - O objeto Request do Express
+                 * @param {import('express').Response} res - O objeto Response do Express
+                 */
                 callback(req, res);
             }
         });
