2141a96458619c1178b69de8743a943d60f06a8e
eomuhzin <pessoalmurillofrazao@gmail.com>
2024-01-23 20:46:58 -0300

    commit

--- public/handler.js
+++ public/handler.js
@@ -1,66 +1,59 @@
-function handlerInit(boolean) {
-    if(boolean) {
-        changePage(window.location.href)
-    }
-    const botoes = document.querySelectorAll('a');
-
-    botoes.forEach(botao => {
-        if (botao.id !== 'form') {
-            botao.addEventListener('click', event => {
-                event.preventDefault();
-                const url = botao.getAttribute('href');
-                changePage(url)
+function a(b) {
+    if(b) c(window.location.href);
+    const d = document.querySelectorAll('a');
+    d.forEach(e => {
+        if (e.id !== 'form') {
+            e.addEventListener('click', f => {
+                f.preventDefault();
+                const g = e.getAttribute('href');
+                c(g);
             });
         }
     });
 }
 
-
-function changePage(url) {
-    console.log(url)
-    fetch(url + "?javascript=true", {headers: new Headers({"javascript": "JavaScript-SPA"})})
-        .then(response => response.text())
-        .then(html => {
-            const oldScripts = document.querySelectorAll('script');
-            oldScripts.forEach(script => script.remove());
-            document.documentElement.innerHTML = html;
-
-                const newScripts = document.querySelectorAll('script');
-                newScripts.forEach(script => {
-                    if (script.src) {
-                        const newScript = document.createElement('script');
-                        newScript.src = script.src;
-                        document.body.appendChild(newScript);
-                    } else {
-                        const content = script.textContent;
-                        new Function(content)();
+function c(h) {
+    fetch(h + "?javascript=true", {headers: new Headers({"javascript": "JavaScript-SPA"})})
+        .then(i => i.text())
+        .then(j => {
+            const k = document.querySelectorAll('script');
+            k.forEach(l => l.remove());
+            document.documentElement.innerHTML = j;
+            const m = document.querySelectorAll('script');
+            m.forEach(n => {
+                if (n.src) {
+                    const o = document.createElement('script');
+                    o.src = n.src;
+                    document.body.appendChild(o);
+                } else {
+                    const p = n.textContent;
+                    new Function(p)();
+                }
+            });
+            if(!window.location.href.endsWith(h)) {
+                window.history.pushState({ h }, '', h);
+            }
+            setTimeout(() => {
+                const q = document.querySelectorAll('body');
+                q.forEach(r =>{
+                    if(!r.onload.toString().includes("a(true)")) {
+                        r.onload();
                     }
                 });
-
-            if(!window.location.href.endsWith(url)) {
-                window.history.pushState({ url }, '', url);
-            }
-            setTimeout(() =>{
-                const bodys = document.querySelectorAll('body');
-                bodys.forEach(body =>{
-                    if(!body.onload.toString().includes("handlerInit(true)")) {
-                        body.onload()
-                    }
-                })
-                handlerInit(false)
-            }, 250)
+                a(false);
+                console.clear();
+            }, 100);
         })
-        .catch(error => {
-            console.error(error);
+        .catch(s => {
+            console.error(s);
         });
 }
 
-
-window.addEventListener('popstate', function(event) {
-    event.preventDefault()
-    changePage(location.href)
+window.addEventListener('popstate', function(t) {
+    t.preventDefault();
+    c(location.href);
 });
 
 window.onload = function () {
-    handlerInit(true)
+    a(true);
 }
